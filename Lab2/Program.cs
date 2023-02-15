3c74ac2920af5d70235277bf89e2ad2d5891c3dc
Daniil Borovoy <daniilborovoy42@gmail.com>
2023-02-15 20:14:33 +0300

    build: init project

---
+++ Lab2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №2 - Наследование");
+        }
+    }
+}
